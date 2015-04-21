6a5bcf269e2ed98303288e90fc03c7ef4f7869ed
Unsalted <nick@unsalted.nu>
2015-04-21 08:55:37 -0700

    setup redis client with test

--- server.js
+++ server.js
@@ -12,21 +12,80 @@ app.disable('x-powered-by');
 var database = function(){
 
   var cnct = this;
+  var client;
 
   cnct.setupVariables = function(){
 
-    cnct.host = process.env.OPENSHIFT_REDIS_DB_HOST;
-    cnct.port = process.env.OPENSHIFT_REDIS_DB_PORT || 6379;
+    cnct.host = process.env.OPENSHIFT_REDIS_HOST;
+    cnct.port = process.env.OPENSHIFT_REDIS_PORT || 6379;
+    cnct.pass = process.env.REDIS_PASSWORD;
 
     if (typeof cnct.host === "undefined") {
-
         console.warn('No OPENSHIFT_REDIS_DB_HOST var, using 127.0.0.1');
         cnct.host = "127.0.0.1";
     };
 
   }
 
-  
+  cnct.createClient = function(){
+
+    client = redis.createClient(cnct.port, cnct.host);
+
+    client.on('connect', function() {
+      console.log('connected');
+    });
+
+    client.on("error", function (err) {
+        console.log("Error " + err);
+    });
+
+    if (typeof cnct.pass != "undefined") {
+        client.auth(cnct.pass);
+    } else {
+        console.warn('NO AUTH REQUIRED');
+    }
+
+  }
+
+  cnct.clientAuth = function(){
+
+    if (typeof cnct.pass != "undefined") {
+        console.warn('NO AUTH REQUIRED');
+        client.auth(cnct.pass);
+    };
+
+  }
+
+  cnct.testClient = function(){
+    var rep;
+
+    client.set("test", "true", function(err, reply) {
+
+      console.log(reply);
+
+    });
+
+    client.get("test", function(err, reply) {
+
+      console.log(reply);
+      rep = reply;
+
+      if(rep === "true"){
+        console.warn("positive test");
+      }
+
+    });
+
+    client.del("test", function(err, reply) {
+
+      console.log(reply);
+      console.warn("deleted test entry");
+
+    });
+
+  }
+
+
 }
 
 var server = function(){
@@ -102,6 +161,9 @@ Start.initializeServer();
 
 var Connect = new database();
 Connect.setupVariables();
+Connect.createClient();
+Connect.testClient();
+
 
 
 
